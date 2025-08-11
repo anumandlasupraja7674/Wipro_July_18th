7d00c2f1f953ab059e9d3a87346f5d3e3a034761
anumandlasupraja <anumandlasupraja7674>
2025-08-11 17:58:30 +0530

    add pgm

---
+++ src/newprogram/Newprogramjava.java
@@ -0,0 +1,11 @@
+package newprogram;
+
+public class Newprogramjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New java program");
+
+	}
+
+}
