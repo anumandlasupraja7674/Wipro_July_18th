6cecbe2c0d6a5375725500cd99c99f822f713eab
anumandlasupraja <anumandlasupraja7674@gmail.com>
2025-08-18 13:08:48 +0530

    Add files via upload

---
+++ src/SELENIUM/src/test/java/com/wipro/selenium/InvokeFirefoxBrowser.java
@@ -0,0 +1,26 @@
+package com.wipro.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+
+public class InvokeFirefoxBrowser {
+
+	public static void main(String[] args) {
+		// Set path to EdgeDriver executable
+        System.setProperty("webdriver.edge.driver", "C://Users//Supraja//Downloads//geckodriver-v0.35.0-win-aarch64//geckodriver.exe");
+        // Create EdgeDriver instance
+        WebDriver driver = new FirefoxDriver();
+        // Open a webpage
+        //driver.get("https://www.google.com");
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+
+	}
+
+}
