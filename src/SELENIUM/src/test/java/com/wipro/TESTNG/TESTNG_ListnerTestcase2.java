c1297b05797ab36439bc05b661275118f261b5d2
anumandlasupraja <anumandlasupraja7674@gmail.com>
2025-08-22 10:30:47 +0530

    new added files

---
+++ src/SELENIUM/src/test/java/com/wipro/TESTNG/TESTNG_ListnerTestcase2.java
@@ -0,0 +1,59 @@
+package com.wipro.TESTNG;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TESTNG_ListnerTestcase2 {
+    
+	//private static final Logger log = LoggerHelper.getLogger(TestNG_ListnerTestcase.class);
+	
+	//By using this we don't have to create LoggerHelper.java
+	private static final Logger log = LogManager.getLogger(TestNG_ListnerTestcase.class);
+    @Test
+    public void login() throws InterruptedException {
+        
+        log.info("Starting login test...");
+
+        ChromeOptions chromeOption = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(chromeOption);
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+
+        log.info("Opened browser and navigated to saucedemo");
+
+        Thread.sleep(2000);
+
+        // Enter text in username
+        WebElement username = driver.findElement(By.name("user-name"));
+        username.sendKeys("standard_user");
+        log.info("Entered username");
+
+        Thread.sleep(2000);
+
+        // Enter text in password
+        WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
+        password.sendKeys("secret_sauce");
+        log.info("Entered password");
+
+        Thread.sleep(2000);
+
+        // Click on login button
+        WebElement login_button = driver.findElement(By.xpath("//input[@type='submit']"));
+        login_button.click();
+        log.info("Clicked login button");
+
+        Thread.sleep(2000);
+        driver.quit();
+        log.info("Browser closed");
+    }
+}
